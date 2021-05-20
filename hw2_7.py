26ef980475defdc733b71d86f0af1449af0fc9b8
AlmazKeldibekov <81243143+AlmazKeldibekov@users.noreply.github.com>
2021-05-20 12:24:03 +0600

    homework2_7

---
+++ hw2_7.py
@@ -0,0 +1,36 @@
+# class Man:
+#     name = 'Asan'
+#     age = 24
+#     weight = 86
+#     def info(self):
+#         print(f'Name:{self.name}----Height:{self.age}----Weight:{self.weight}')
+# c = Man()
+# c.info()
+# class Monkey:
+#     max_age = 12
+#     loves_bananas = True
+#
+#     def climb(self):
+# 	    print('I am climbing the tree')
+#
+# c = Monkey()
+#
+# c.climb()
+# print(c.max_age , c.loves_bananas)
+#
+# c1 = Monkey()
+# c1.climb()
+# print(c1.max_age , c1.loves_bananas)
+
+# class Person:
+#     def __init__(self,name,age,gender):
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+#
+#     def old(self,years):
+#         a = self.age
+#         sum_ = a + years
+#         print(f'Через {years} лет {self.name} исполнится {sum_} лет/год' )
+# c = Person('Abu' , 33 , 'Male')
+# c.old(10)
